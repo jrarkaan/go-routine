2bb482daeb23709ed29f895c47460fa798da4d0f
jrarkaan <rakajanitraa@hotmail.com>
2022-09-26 22:12:35 +0700

    race condition

--- channel_test.go
+++ channel_test.go
@@ -141,3 +141,17 @@ func TestSelectDefaultChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestRaceCondition(t *testing.T) {
+	x := 0
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				x += 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
