e43106f6f231b680e7a54dc2366d168fd4d1376c
jrarkaan <rakajanitraa@hotmail.com>
2022-09-27 11:07:13 +0700

    mutex rwm

---
+++ mutex_test.go
@@ -0,0 +1,58 @@
+package go_routine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x += 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
+
+type BankAccount struct {
+	RWMUtex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMUtex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMUtex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMUtex.RLock()
+	balance := account.Balance
+	account.RWMUtex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total balance: ", account.GetBalance())
+}
