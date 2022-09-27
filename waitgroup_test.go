0db36d9eac0d9126e12c564c2790bf268c3af045
jrarkaan <rakajanitraa@hotmail.com>
2022-09-27 11:37:24 +0700

    wait group

---
+++ waitgroup_test.go
@@ -0,0 +1,28 @@
+package go_routine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsync(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsync(group)
+	}
+
+	group.Wait()
+	fmt.Println("Selesai")
+}
